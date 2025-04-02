ce22d9263834b6f976294ab2528568b090792f98
RodrigoSpano <spanorodri@gmail.com>
2025-04-02 19:01:48 -0300

    migrate from mongo to postgres

--- src/reminders/dto/update-reminder.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReminderDto } from './create-reminder.dto';
-
-export class UpdateReminderDto extends PartialType(CreateReminderDto) {
-  id: number;
-}
